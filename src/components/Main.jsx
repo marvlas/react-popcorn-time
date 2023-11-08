9aedc375aa36d126cfb99ee9a4dac3d8c71864e2
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-08 15:40:08 +0100

    add examples of conditional rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,22 +3,11 @@ import movies from "../data/movies.json";
 
 function Main(){
 
-    // const [something, setSomething] = useState(initialValue);
-
     const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
-
     const deleteMovie = (movieId) => {
-
-
         const newList = moviesToDisplay.filter( (movieDetails) => {
             return movieDetails.id !== movieId;
-            
-            // if(movieDetails.id === movieId){
-            //     return false; // will not go to the final array
-            // } else {
-            //     return true; // will be in the final array
-            // }
         });
         
         // moviesToDisplay = newList; // NEVER UPDATE STATE DIRECTLY !!
@@ -26,15 +15,32 @@ function Main(){
         setMoviesToDisplay(newList);
     }
 
+    let message = "";
+    if(moviesToDisplay.length){
+        message = <h1>List of movies</h1>
+    } else {
+        message = <h1>Sorry, no movies to display</h1>
+    }
+
     return (
         <>
-            <h1>List of movies</h1>
+
+            {message}
 
             {moviesToDisplay.map(function(movieObj, index){
                 return(
                     <div key={movieObj.id}>
+
+                        {movieObj.imgURL 
+                            ? <img src={movieObj.imgURL}  /> 
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" />
+                        }
+
                         <h2>{movieObj.title}</h2>
                         <p>Rating: {movieObj.rating}</p>
+
+                        { movieObj.rating > 8 && <p>RECOMMENDED</p>}
+                        
 
                         <button onClick={function(){ deleteMovie(movieObj.id) }}>Delete</button>
                         
