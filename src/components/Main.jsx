31307bc0c7053f5b48a3bf34f654f68c5dd95014
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-08 15:39:00 +0100

    functionality to delete movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import movies from "../data/movies.json";
 
 function Main(){
+
+    // const [something, setSomething] = useState(initialValue);
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+
+    const deleteMovie = (movieId) => {
+
+
+        const newList = moviesToDisplay.filter( (movieDetails) => {
+            return movieDetails.id !== movieId;
+            
+            // if(movieDetails.id === movieId){
+            //     return false; // will not go to the final array
+            // } else {
+            //     return true; // will be in the final array
+            // }
+        });
+        
+        // moviesToDisplay = newList; // NEVER UPDATE STATE DIRECTLY !!
+        // moviesToDisplay.pop(); // NEVER UPDATE STATE DIRECTLY !!
+        setMoviesToDisplay(newList);
+    }
 
     return (
         <>
             <h1>List of movies</h1>
 
-            {movies.map(function(movieObj, index){
+            {moviesToDisplay.map(function(movieObj, index){
                 return(
                     <div key={movieObj.id}>
                         <h2>{movieObj.title}</h2>
                         <p>Rating: {movieObj.rating}</p>
 
-                        <button>Delete this movie</button>
+                        <button onClick={function(){ deleteMovie(movieObj.id) }}>Delete</button>
+                        
                     </div>
                 );
             })}
